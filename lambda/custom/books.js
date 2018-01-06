a997a21514a6461682847aa6ac3f25b1974195d3
Fellipe Brito <fellipe@devlandia.net>
2018-01-06 00:22:47 -0800

    Add Question/Answers

---
+++ lambda/custom/books.js
@@ -0,0 +1,9 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent']*/
+module.exports = {
+    'BOOKS': {
+        'jeremiah': 'The Book of Jeremiah has 33,002 words',
+        'genesis': 'The Book of Genesis has 32,046 words'
+    }
+};
--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,45 +1,80 @@
 'use strict';
 const Alexa = require('alexa-sdk');
+const myBooks = require('./books');
 
 const APP_ID = undefined;
 require('./facts.js');
 
-//
 //====
 exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.appId = APP_ID;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  var alexa = Alexa.handler(event, context);
+  alexa.appId = APP_ID;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
 
 const handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetNewFactIntent');
-    },
-    'GetNewFactIntent': function () {
-        const factArr = data;
-        const factIndex = Math.floor(Math.random() * factArr.length);
-        const randomFact = factArr[factIndex];
-        const speechOutput = GET_FACT_MESSAGE + randomFact;
+  'LaunchRequest': function () {
+    this.emit('GetNewFactIntent');
+  },
+  'GetNewFactIntent': function () {
+    const factArr = data;
+    const factIndex = Math.floor(Math.random() * factArr.length);
+    const randomFact = factArr[factIndex];
+    const speechOutput = GET_FACT_MESSAGE + randomFact;
 
-        this.response.cardRenderer(SKILL_NAME, randomFact);
-        this.response.speak(speechOutput);
-        this.emit(':responseReady');
-    },
-    'AMAZON.HelpIntent': function () {
-        const speechOutput = HELP_MESSAGE;
-        const reprompt = HELP_REPROMPT;
+    this.response.cardRenderer(SKILL_NAME, randomFact);
+    this.response.speak(speechOutput);
+    this.emit(':responseReady');
+  },
+  'GetAnswerIntent': function () {
+    const itemSlot = this.event.request.intent.slots.book;
+    let itemName;
+    if (itemSlot && itemSlot.value) {
+      itemName = itemSlot.value.toLowerCase();
+    }
 
-        this.response.speak(speechOutput).listen(reprompt);
-        this.emit(':responseReady');
-    },
-    'AMAZON.CancelIntent': function () {
-        this.response.speak(STOP_MESSAGE);
-        this.emit(':responseReady');
-    },
-    'AMAZON.StopIntent': function () {
-        this.response.speak(STOP_MESSAGE);
-        this.emit(':responseReady');
-    },
+    const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
+    const book = myBooks['BOOKS'][itemName];
+
+    if (book) {
+      this.attributes.speechOutput = book;
+      this.attributes.repromptSpeech = this.t('BOOK_REPEAT_MESSAGE');
+
+      this.response.speak(book).listen(this.attributes.repromptSpeech);
+      this.response.cardRenderer(cardTitle, book);
+      this.emit(':responseReady');
+    } else {
+      let speechOutput = this.t('BOOK_NOT_FOUND_MESSAGE');
+      const repromptSpeech = this.t('BOOK_NOT_FOUND_REPROMPT');
+      if (itemName) {
+        speechOutput += this.t('BOOK_NOT_FOUND_WITH_ITEM_NAME', itemName);
+      } else {
+        speechOutput += this.t('BOOK_NOT_FOUND_WITHOUT_ITEM_NAME');
+      }
+      speechOutput += repromptSpeech;
+
+
+      this.attributes.speechOutput = speechOutput;
+      this.attributes.repromptSpeech = repromptSpeech;
+
+      this.response.speak(speechOutput).listen(repromptSpeech);
+      this.emit(':responseReady');
+    }
+  },
+  'AMAZON.HelpIntent': function () {
+    const speechOutput = HELP_MESSAGE;
+    const reprompt = HELP_REPROMPT;
+
+    this.response.speak(speechOutput).listen(reprompt);
+    this.emit(':responseReady');
+  },
+  'AMAZON.CancelIntent': function () {
+    this.response.speak(STOP_MESSAGE);
+    this.emit(':responseReady');
+  },
+  'AMAZON.StopIntent': function () {
+    this.response.speak(STOP_MESSAGE);
+    this.emit(':responseReady');
+  },
 };
